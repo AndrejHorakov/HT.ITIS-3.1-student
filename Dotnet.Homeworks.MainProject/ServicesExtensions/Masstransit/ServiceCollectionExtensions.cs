1e2d5c500c956dd5db90d2dcc5baee8f79d96d6c
AndrejHoryakov <andrejhorakov47232@gmail.com>
2023-12-20 23:55:22 +0300

    hw 5

--- Dotnet.Homeworks.MainProject/ServicesExtensions/Masstransit/ServiceCollectionExtensions.cs
+++ Dotnet.Homeworks.MainProject/ServicesExtensions/Masstransit/ServiceCollectionExtensions.cs
@@ -1,12 +1,35 @@
 using Dotnet.Homeworks.MainProject.Configuration;
+using MassTransit;
 
 namespace Dotnet.Homeworks.MainProject.ServicesExtensions.Masstransit;
 
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddMasstransitRabbitMq(this IServiceCollection services,
-        RabbitMqConfig rabbitConfiguration)
+        IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        var rabbitConfiguration = new RabbitMqConfig
+        {
+            Hostname = configuration["RabbitMqConfig:Hostname"]!,
+            Password = configuration["RabbitMqConfig:Password"]!,
+            Username = configuration["RabbitMqConfig:Username"]!
+        };
+        Console.WriteLine(rabbitConfiguration.Hostname);
+        Console.WriteLine(rabbitConfiguration.Username);
+        Console.WriteLine(rabbitConfiguration.Password);
+        services.AddMassTransit(cfg =>
+        {
+            cfg.SetKebabCaseEndpointNameFormatter();
+            cfg.UsingRabbitMq((context, config) =>
+            {
+                config.Host(rabbitConfiguration.Hostname, "/",rc =>
+                {
+                    rc.Username(rabbitConfiguration.Username);
+                    rc.Password(rabbitConfiguration.Password);
+                });
+                config.ConfigureEndpoints(context);
+            });
+        });
+        return services;
     }
 }
